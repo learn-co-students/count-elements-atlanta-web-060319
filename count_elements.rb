09dba8ee2d84e94ba2acc5f43d0983db9ad527a8
github username <github email address>
2019-06-03 22:42:24 -0400

    Complete

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,11 @@
+require 'pry'
+
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+  array.uniq.each do |element|
+    new_hash[element] = array.count(element)
+  end 
+  new_hash
 end
+
  
